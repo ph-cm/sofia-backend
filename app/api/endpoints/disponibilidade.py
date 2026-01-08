d494ece0b76095b9dac74a2b06ec22e0fbf45d00
Pedro Henrique <trabalho.prog.pedro@gmail.com>
2026-01-08 10:52:42 -0300

    Fix: ajustando caminho do banco

--- app/api/endpoints/disponibilidade.py
+++ app/api/endpoints/disponibilidade.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
-from db.session import get_db
+from app.db.session import get_db
 from models import ProfissionalDisponibilidade
 
 router = APIRouter()
