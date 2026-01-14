5421327517c7331f8f33f23c6165ac3b27a95015
Pedro Henrique <trabalho.prog.pedro@gmail.com>
2026-01-14 19:32:00 -0300

    Fix: adicionando id no update

--- app/api/endpoints/users.py
+++ app/api/endpoints/users.py
@@ -27,15 +27,14 @@ def login(data: UserLogin, db: Session = Depends(get_db)):
         raise HTTPException(status_code=401, detail="Invalid credentials")
     return user
 
-@router.put("/update", response_model=UserOut)
-def update_me(
+@router.put("/update/{user_id}", response_model=UserOut)
+def update_user(
+    user_id: int,
     payload: UserUpdate,
     db: Session = Depends(get_db),
-    current_user = Depends(get_current_user),
 ):
-    user = UserService.update_user(db, current_user["id"], payload)
-
+    user = UserService.update_user(db, user_id, payload)
     if not user:
         raise HTTPException(status_code=404, detail="User not found")
-
     return user
+
