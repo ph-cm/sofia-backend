8c4de05e3e5fc3bf26af92cfc02312b350c3c2f9
Pedro Henrique <trabalho.prog.pedro@gmail.com>
2026-01-14 10:59:43 -0300

    Fix: ajustando o schema para int

--- app/schemas/conversation_context.py
+++ app/schemas/conversation_context.py
@@ -2,8 +2,8 @@ from pydantic import BaseModel
 
 class ConversationContextCreate(BaseModel):
     conversation_id: str
-    user_id: str
+    user_id: int
 
 class ConversationContextResponse(BaseModel):
     conversation_id: str
-    user_id: str
+    user_id: int
