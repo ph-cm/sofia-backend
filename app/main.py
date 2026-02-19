6d3b7a1f6fcb4bd7a8b446b30180090767a623fd
Pedro Henrique <trabalho.prog.pedro@gmail.com>
2026-02-19 10:30:49 -0300

    fix

--- app/main.py
+++ app/main.py
@@ -19,15 +19,23 @@ app = FastAPI(
     description="Backend para gerenciamento de instâncias e integrações.",
     version="0.1.0",
 )
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=[
+#         "http://localhost:5173",
+#         "http://127.0.0.1:5173",
+#         "http://localhost:3000",
+#         "http://127.0.0.1:3000",
+#     ],
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )   
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173",
-        "http://127.0.0.1:5173",
-        "http://localhost:3000",
-        "http://127.0.0.1:3000",
-    ],
-    allow_credentials=True,
+    allow_origins=["*"],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
